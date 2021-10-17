19dcabdfec7beffbffc6f48b085bec53b24bc0a2
Aishwarya Mali <aishwaryamali916@gmail.com>
2021-10-17 10:51:46 +0530

    Negative_not_allowed_And_total_return

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -17,3 +17,25 @@ class StringCalculator {
     	}
 
 }
+    private int sum(String[] numbers) {
+    	int total = 0;
+    	String negString = "";
+    	for(String number : numbers) {
+    		if(toInt(number) < 0) {
+    			if(negString.equals(""))
+    				negString = number;
+    			else
+    				negString += ("," + number);
+    			
+    		}
+    		if(toInt(number) < 1000)
+    			total += toInt(number);
+    		
+    	}
+    	if(!negString.equals("")) {
+    		throw new IIIegalArgumentException("Negatives not allowed: " + negString);
+    		
+    	}
+    	return total;
+    	
+    }
